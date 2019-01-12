f7776c092bf0f6b412cac02a3f9e638441ac3ba4
michaelmcmullin <m.mcmullin@outlook.com>
2019-01-12 15:33:26 +0000

    Adds empty fragment code and layout for each tab

---
+++ app/src/main/java/com/sda/mcmullm2/assignment4/Tab1Fragment.java
@@ -0,0 +1,19 @@
+package com.sda.mcmullm2.assignment4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sda.mcmullm2.assignment3.R;
+
+public class Tab1Fragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_tab1, container, false);
+  }
+
+}
