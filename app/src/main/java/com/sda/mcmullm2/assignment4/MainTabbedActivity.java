62fd7f6ef0dbe7400c3034081a013d13d0b47312
michaelmcmullin <m.mcmullin@outlook.com>
2019-01-21 05:59:29 +0000

    Adds reference to fragments in main activity
    By giving the main activity access to each fragment, it should be
    possible to obtain information from each tab to construct an order.

--- app/src/main/java/com/sda/mcmullm2/assignment4/MainTabbedActivity.java
+++ app/src/main/java/com/sda/mcmullm2/assignment4/MainTabbedActivity.java
@@ -1,23 +1,26 @@
 package com.sda.mcmullm2.assignment4;
 
 import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.TabLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainTabbedActivity extends AppCompatActivity {
+  private final String TAG = "Assign4";
+
   private final int HOME_TAB_INDEX = 0;
   String[] tabTitles;
   SharedPreferences prefs;
-  private String customerName;
-  private String address;
-  private int deliveryTime;
-  private boolean isCollection;
+  private FragmentProducts productsFragment;
+  private FragmentOrders ordersFragment;
+  private FragmentCollection collectionFragment;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -57,27 +60,56 @@ public class MainTabbedActivity extends AppCompatActivity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
+    Log.i(TAG, "Starting onOptionsItemSelected method");
+
     switch (item.getItemId()) {
       case R.id.menuitem_send:
-        setPreferences();
-        EmailSummary summary = new EmailSummary(this.getApplicationContext(), prefs);
-        Toast.makeText(this, summary.toString(), // getString(R.string.button_place_order),
-            Toast.LENGTH_SHORT).show();
-        return true;
+        Log.i(TAG, "menuitem_send");
+        if (setPreferences()) {
+          EmailSummary summary = new EmailSummary(this.getApplicationContext(), prefs);
+          // TODO: open email app
+          Toast.makeText(this, summary.toString(), // getString(R.string.button_place_order),
+              Toast.LENGTH_SHORT).show();
+          return true;
+        }
     }
     return false;
   }
 
-  private void setPreferences() {
-    if (tabTitles == null && tabTitles.length < 4) return;
+  /**
+   * https://developer.android.com/training/basics/fragments/communicating
+   *
+   * @param fragment
+   */
+  @Override
+  public void onAttachFragment(Fragment fragment) {
+    super.onAttachFragment(fragment);
 
-    // https://learnpainless.com/android/how-to-get-fragment-from-viewpager-android
-    FragmentOrders ordersFragment = (FragmentOrders) getSupportFragmentManager().findFragmentByTag("android:switcher:" + R.id.pager + ":" + 2);
-
-    if (prefs != null) {
-      SharedPreferences.Editor editor = prefs.edit();
-      editor.putString("CustomerName", ordersFragment.getTest());
-      editor.commit();
+    if (productsFragment == null && fragment instanceof FragmentProducts) {
+      Log.i(TAG, "Products fragment added to main activity");
+      productsFragment = (FragmentProducts) fragment;
+    } else if (ordersFragment == null && fragment instanceof FragmentOrders) {
+      Log.i(TAG, "Orders fragment added to main activity");
+      ordersFragment = (FragmentOrders) fragment;
+    } else if (collectionFragment == null && fragment instanceof FragmentCollection) {
+      Log.i(TAG, "Collection fragment added to main activity");
+      collectionFragment = (FragmentCollection) fragment;
     }
+  }
+
+  private boolean setPreferences() {
+    Log.i(TAG, "Setting preferences");
+
+    if (prefs == null) {
+      Toast.makeText(this, getString(R.string.error_no_prefs), Toast.LENGTH_SHORT).show();
+      return false;
+    }
+    if (productsFragment == null) return false;
+    if (ordersFragment == null) return false;
+
+    SharedPreferences.Editor editor = prefs.edit();
+    editor.putString("CustomerName", ordersFragment.getTest());
+    editor.commit();
+    return true;
   }
 }
